2394044e589bd4d22d81d95517af6cf2a752b984
makifcay <makifcay@outlook.com>
2020-11-15 02:18:46 +0300

    added options, view

---
+++ src/views/Applicant.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="applicant">
+    <Navbar/>
+    <div class="container text-center">
+      <div class="btn-group" role="group" aria-label="Basic example">
+        <router-link class="btn btn-success" router-tag="button" to="/applicant/register">
+          Yeni Kayıt
+        </router-link>
+        <router-link class="btn btn-primary" router-tag="button" to="/applicant/edit">
+          Kaydı Düzenle
+        </router-link>
+        <router-view/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  name: "Applicant",
+  components: {
+    Navbar
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  margin: 10px auto;
+  .btn-group {
+    margin: 10px auto;
+  }
+}
+</style>
