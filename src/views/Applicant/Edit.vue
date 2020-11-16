11fb51b516b5a45335b38e7e7d22dbabf5f30282
makifcay <makifcay@outlook.com>
2020-11-16 20:27:31 +0300

    added ApplicantNav.vue

--- src/views/Applicant/Edit.vue
+++ src/views/Applicant/Edit.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="edit">
     <Navbar/>
+    <ApplicantNav/>
 
     <div class="container text-center">
       <h1>Edit</h1>
@@ -11,11 +12,13 @@
 <script>
 // @ is an alias to /src
 import Navbar from "@/components/Navbar.vue";
+import ApplicantNav from "@/components/ApplicantNav.vue";
 
 export default {
   name: "Register",
   components: {
-    Navbar
+    Navbar,
+    ApplicantNav
   }
 };
 </script>
